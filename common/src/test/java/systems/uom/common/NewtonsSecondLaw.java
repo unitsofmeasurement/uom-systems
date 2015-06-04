81adef0118f56a86b54a1f5f6084678b5f0349aa
Werner Keil <werner.keil@gmx.net>
2015-06-04 23:11:05 +0200

    2: Add common module
    Task-Url:
    https://github.com/unitsofmeasurement/uom-systems/issues/issues/issue/2

---
+++ common/src/test/java/systems/uom/common/NewtonsSecondLaw.java
@@ -0,0 +1,53 @@
+/**
+ *  Unit-API - Units of Measurement API for Java
+ *  Copyright (c) 2005-2015, Jean-Marie Dautelle, Werner Keil, V2COM.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-363, Unit-API nor the names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package systems.uom.common;
+
+import javax.measure.quantity.Force;
+
+import si.uom.impl.quantity.AccelerationAmount;
+import si.uom.impl.quantity.ForceAmount;
+import si.uom.impl.quantity.MassAmount;
+import si.uom.impl.SI;
+
+/**
+ *
+ * @author Chris Senior
+ * @author Werner Keil
+ *
+ */
+class NewtonsSecondLaw {
+
+	public static final ForceAmount calculateForce(MassAmount m, AccelerationAmount a) {
+		double m_kg = m.doubleValue(SI.KILOGRAM);
+		double a_si = a.doubleValue(SI.METRES_PER_SQUARE_SECOND);
+
+		return new ForceAmount(m_kg * a_si, SI.NEWTON);
+	}
+}
