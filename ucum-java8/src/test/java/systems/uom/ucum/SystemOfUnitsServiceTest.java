2dabc7befc03f3c119c78238bd86094072fd747c
Werner Keil <werner.keil@gmx.net>
2017-03-19 00:43:18 +0100

    # WARNING: head commit changed in the meantime
    55: Consider using SPI in AbstractUnit 
    
    Task-Url: https://github.com/unitsofmeasurement/unit-ri/issues/issues/55

--- ucum-java8/src/test/java/systems/uom/ucum/SystemOfUnitsServiceTest.java
+++ ucum-java8/src/test/java/systems/uom/ucum/SystemOfUnitsServiceTest.java
@@ -39,7 +39,7 @@ import org.junit.Test;
 
 public class SystemOfUnitsServiceTest {
     private static final String EXPECTED_SYSTEM_NAME = "Unified Code for Units of Measure";
-    private static final int NUM_OF_UNITS = 215;
+    private static final int NUM_OF_UNITS = 213;
 
     private static SystemOfUnitsService defaultService;
 
