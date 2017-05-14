07c7466f8b278b70d07c39523644abcc6e2614d1
Werner Keil <werner.keil@gmx.net>
2017-05-15 00:30:07 +0200

    97: UCUMFormat issues formatting multiples or submultiples
    Task-Url: https://github.com/unitsofmeasurement/uom-systems/issues/issues/97

--- unicode-java8/src/test/java/systems/uom/unicode/SystemOfUnitsServiceTest.java
+++ unicode-java8/src/test/java/systems/uom/unicode/SystemOfUnitsServiceTest.java
@@ -1,6 +1,6 @@
 /*
- *  Unit-API - Units of Measurement API for Java
- *  Copyright (c) 2005-2015, Jean-Marie Dautelle, Werner Keil, V2COM.
+ * Units of Measurement Systems for Java
+ * Copyright (c) 2005-2017, Jean-Marie Dautelle, Werner Keil and others.
  *
  * All rights reserved.
  *
